deb9c1b6de2dea118572e8efa472e1f7de62018a
fnmabreu@gmail.com <fnmabreu@gmail.com>
2021-11-28 11:17:24 +0000

    Enable JMS AutoConfig

--- src/main/java/guru/sfg/beer/order/service/BeerOrderServiceApplication.java
+++ src/main/java/guru/sfg/beer/order/service/BeerOrderServiceApplication.java
@@ -1,10 +1,7 @@
 package guru.sfg.beer.order.service;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jms.artemis.ArtemisAutoConfiguration;
 
-@SpringBootApplication(exclude = ArtemisAutoConfiguration.class)
 public class BeerOrderServiceApplication {
 
     public static void main(String[] args) {
